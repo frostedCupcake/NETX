28dac9d3727d443a666fbfb73a7cb028a0625e62
jaswanth <jaswanthbeere@gmail.com>
2022-10-31 19:17:26 +0530

    responsive nav bar fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const hidden_class = "hidden w-full md:block md:w-auto";
+  const normal_class = " w-full md:block md:w-auto";
   const router = useRouter();
+  const [hidden, setHidden] = useState(hidden_class);
+
   const active_class =
     "block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0";
   const not_active_class =
     "block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0";
+
   return (
     <nav className="bg-white border-2 px-2 sm:px-4 py-2.5 rounded sticky top-0 z-50">
       <div className="container flex flex-wrap justify-between items-center mx-auto relative">
@@ -21,26 +26,33 @@ const Navbar = () => {
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
+          class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
           aria-expanded="false"
+          onClick={() => {
+            setHidden(normal_class);
+            let temp = normal_class;
+            normal_class = hidden_class;
+            hidden_class = temp;
+          }}
         >
-          <span className="sr-only">Open main menu</span>
+          <span class="sr-only">Open main menu</span>
           <svg
-            className="w-6 h-6"
+            class="w-6 h-6"
             aria-hidden="true"
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fillRule="evenodd"
+              fill-rule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
+              clip-rule="evenodd"
             ></path>
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+
+        <div className={hidden} id="navbar-default">
           <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white ">
             <li>
               <Link href="/">
